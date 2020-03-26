117fd5aeef3f2c924c1c36ab8dc369c0dc83ab76
aghsk <aghski@gmail.com>
2020-03-26 20:15:15 +0100

    Codigo parte 1 hecho. boton Favorito.

--- app/src/main/java/com/example/repostador_2020/MainActivity.java
+++ app/src/main/java/com/example/repostador_2020/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.repostador_2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Intent ventanaRepostaje = new Intent(this, Repostaje.class);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               startActivity(ventanaRepostaje);
+            }
+        });
     }
 }
